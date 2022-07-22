640eb69a1440a0438ad5ca05c138a0f457b49053
Daniel Kang <dk@cleinsoft.com>
2022-07-22 11:17:27 +0900

    window-in arg option add

--- datautils.py
+++ datautils.py
@@ -68,7 +68,7 @@ def load_UCR(dataset):
         'UMD'
     ]:
         return train[..., np.newaxis], train_labels, test[..., np.newaxis], test_labels
-    
+
     mean = np.nanmean(train)
     std = np.nanstd(train)
     train = (train - mean) / std
@@ -79,7 +79,7 @@ def load_UCR(dataset):
 def load_UEA(dataset):
     train_data = loadarff(f'datasets/UEA/{dataset}/{dataset}_TRAIN.arff')[0]
     test_data = loadarff(f'datasets/UEA/{dataset}/{dataset}_TEST.arff')[0]
-    
+
     def extract_data(data):
         res_data = []
         res_labels = []
@@ -89,31 +89,31 @@ def load_UEA(dataset):
             res_data.append(t_data)
             res_labels.append(t_label)
         return np.array(res_data).swapaxes(1, 2), np.array(res_labels)
-    
+
     train_X, train_y = extract_data(train_data)
     test_X, test_y = extract_data(test_data)
-    
+
     scaler = StandardScaler()
     scaler.fit(train_X.reshape(-1, train_X.shape[-1]))
     train_X = scaler.transform(train_X.reshape(-1, train_X.shape[-1])).reshape(train_X.shape)
     test_X = scaler.transform(test_X.reshape(-1, test_X.shape[-1])).reshape(test_X.shape)
-    
+
     labels = np.unique(train_y)
     transform = { k : i for i, k in enumerate(labels)}
     train_y = np.vectorize(transform.get)(train_y)
     test_y = np.vectorize(transform.get)(test_y)
     return train_X, train_y, test_X, test_y
-    
-    
+
+
 def load_forecast_npy(name, univar=False):
-    data = np.load(f'datasets/{name}.npy')    
+    data = np.load(f'datasets/{name}.npy')
     if univar:
         data = data[: -1:]
-        
+
     train_slice = slice(None, int(0.6 * len(data)))
     valid_slice = slice(int(0.6 * len(data)), int(0.8 * len(data)))
     test_slice = slice(int(0.8 * len(data)), None)
-    
+
     scaler = StandardScaler().fit(data[train_slice])
     data = scaler.transform(data)
     data = np.expand_dims(data, 0)
@@ -134,11 +134,11 @@ def _get_time_features(dt):
     ], axis=1).astype(np.float)
 
 
-def load_forecast_csv(name, univar=False):
+def load_forecast_csv(name, window_in, univar=False):
     data = pd.read_csv(f'datasets/{name}.csv', index_col='date', parse_dates=True)
     dt_embed = _get_time_features(data.index)
     n_covariate_cols = dt_embed.shape[-1]
-    
+
     if univar:
         if name in ('ETTh1', 'ETTh2', 'ETTm1', 'ETTm2'):
             data = data[['OT']]
@@ -146,7 +146,7 @@ def load_forecast_csv(name, univar=False):
             data = data[['MT_001']]
         else:
             data = data.iloc[:, -1:]
-        
+
     data = data.to_numpy()
     if name == 'ETTh1' or name == 'ETTh2':
         train_slice = slice(None, 12*30*24)
@@ -157,27 +157,34 @@ def load_forecast_csv(name, univar=False):
         valid_slice = slice(12*30*24*4, 16*30*24*4)
         test_slice = slice(16*30*24*4, 20*30*24*4)
     else:
-        train_slice = slice(None, int(0.6 * len(data)))
-        valid_slice = slice(int(0.6 * len(data)), int(0.8 * len(data)))
-        test_slice = slice(int(0.8 * len(data)), None)
-    
+        if window_in == 0:
+            train_slice = slice(None, int(0.6 * len(data)))
+            valid_slice = slice(int(0.6 * len(data)), int(0.8 * len(data)))
+            test_slice = slice(int(0.8 * len(data)), None)
+        else:
+            train_slice = slice(None, int(0.8 * len(data))-(672+window_in))
+            valid_slice = slice(int(0.6 * len(data)), int(0.8 * len(data)))
+            test_slice = slice(672+window_in, None)
+            print(test_slice)
+
     scaler = StandardScaler().fit(data[train_slice])
     data = scaler.transform(data)
     if name in ('electricity'):
         data = np.expand_dims(data.T, -1)  # Each variable is an instance rather than a feature
     else:
         data = np.expand_dims(data, 0)
-    
+
     if n_covariate_cols > 0:
         dt_scaler = StandardScaler().fit(dt_embed[train_slice])
         dt_embed = np.expand_dims(dt_scaler.transform(dt_embed), 0)
         data = np.concatenate([np.repeat(dt_embed, data.shape[0], axis=0), data], axis=-1)
-    
+
     if name in ('ETTh1', 'ETTh2', 'electricity'):
         pred_lens = [24, 48, 168, 336, 720]
     else:
-        pred_lens = [24, 48, 96, 288, 672]
-        
+        pred_lens = [24, 48, 168, 336, 672]
+        #pred_lens = [24, 48, 96, 288, 672]
+
     return data, train_slice, valid_slice, test_slice, scaler, pred_lens, n_covariate_cols
 
 
--- train.py
+++ train.py
@@ -38,56 +38,57 @@ if __name__ == '__main__':
     parser.add_argument('--max-threads', type=int, default=None, help='The maximum allowed number of threads used by this process')
     parser.add_argument('--eval', action="store_true", help='Whether to perform evaluation after training')
     parser.add_argument('--irregular', type=float, default=0, help='The ratio of missing observations (defaults to 0)')
+    parser.add_argument('--window-in', type=int, default=0, help='window-in dataset num (defaults to 0 set test set as 20% of total datasets.)')
     args = parser.parse_args()
-    
+
     print("Dataset:", args.dataset)
     print("Arguments:", str(args))
-    
+
     device = init_dl_program(args.gpu, seed=args.seed, max_threads=args.max_threads)
-    
+
     print('Loading data... ', end='')
     if args.loader == 'UCR':
         task_type = 'classification'
         train_data, train_labels, test_data, test_labels = datautils.load_UCR(args.dataset)
-        
+
     elif args.loader == 'UEA':
         task_type = 'classification'
         train_data, train_labels, test_data, test_labels = datautils.load_UEA(args.dataset)
-        
+
     elif args.loader == 'forecast_csv':
         task_type = 'forecasting'
         data, train_slice, valid_slice, test_slice, scaler, pred_lens, n_covariate_cols = datautils.load_forecast_csv(args.dataset)
         train_data = data[:, train_slice]
-        
+
     elif args.loader == 'forecast_csv_univar':
         task_type = 'forecasting'
-        data, train_slice, valid_slice, test_slice, scaler, pred_lens, n_covariate_cols = datautils.load_forecast_csv(args.dataset, univar=True)
+        data, train_slice, valid_slice, test_slice, scaler, pred_lens, n_covariate_cols = datautils.load_forecast_csv(args.dataset, args.window_in, univar=True)
         train_data = data[:, train_slice]
-        
+
     elif args.loader == 'forecast_npy':
         task_type = 'forecasting'
         data, train_slice, valid_slice, test_slice, scaler, pred_lens, n_covariate_cols = datautils.load_forecast_npy(args.dataset)
         train_data = data[:, train_slice]
-        
+
     elif args.loader == 'forecast_npy_univar':
         task_type = 'forecasting'
         data, train_slice, valid_slice, test_slice, scaler, pred_lens, n_covariate_cols = datautils.load_forecast_npy(args.dataset, univar=True)
         train_data = data[:, train_slice]
-        
+
     elif args.loader == 'anomaly':
         task_type = 'anomaly_detection'
         all_train_data, all_train_labels, all_train_timestamps, all_test_data, all_test_labels, all_test_timestamps, delay = datautils.load_anomaly(args.dataset)
         train_data = datautils.gen_ano_train_data(all_train_data)
-        
+
     elif args.loader == 'anomaly_coldstart':
         task_type = 'anomaly_detection_coldstart'
         all_train_data, all_train_labels, all_train_timestamps, all_test_data, all_test_labels, all_test_timestamps, delay = datautils.load_anomaly(args.dataset)
         train_data, _, _, _ = datautils.load_UCR('FordA')
-        
+
     else:
         raise ValueError(f"Unknown loader {args.loader}.")
-        
-        
+
+
     if args.irregular > 0:
         if task_type == 'classification':
             train_data = data_dropout(train_data, args.irregular)
@@ -95,23 +96,23 @@ if __name__ == '__main__':
         else:
             raise ValueError(f"Task type {task_type} is not supported when irregular>0.")
     print('done')
-    
+
     config = dict(
         batch_size=args.batch_size,
         lr=args.lr,
         output_dims=args.repr_dims,
         max_train_length=args.max_train_length
     )
-    
+
     if args.save_every is not None:
         unit = 'epoch' if args.epochs is not None else 'iter'
         config[f'after_{unit}_callback'] = save_checkpoint_callback(args.save_every, unit)
 
     run_dir = 'training/' + args.dataset + '__' + name_with_datetime(args.run_name)
     os.makedirs(run_dir, exist_ok=True)
-    
+
     t = time.time()
-    
+
     model = TS2Vec(
         input_dims=train_data.shape[-1],
         device=device,
